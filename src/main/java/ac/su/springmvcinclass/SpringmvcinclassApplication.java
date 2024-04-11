8cf9943651cc5f06f783f623e5ad2f37a473af86
wild-mental <wild.mental@kidsnote.com>
2024-04-11 17:32:19 +0900

    initial commit

---
+++ src/main/java/ac/su/springmvcinclass/SpringmvcinclassApplication.java
@@ -0,0 +1,13 @@
+package ac.su.springmvcinclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcinclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcinclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/springmvcinclass/SpringmvcinclassApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.springmvcinclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcinclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
