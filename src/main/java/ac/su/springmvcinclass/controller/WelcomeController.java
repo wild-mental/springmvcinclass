785c71fc796136b0d31a2c6703362a52db268cfa
wild-mental <wild.mental@kidsnote.com>
2024-04-11 17:33:33 +0900

    jsp 파일 동작 테스트

---
+++ src/main/java/ac/su/springmvcinclass/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package ac.su.springmvcinclass.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcome() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Site Home</title>
+</head>
+<body>
+<h1>사이트 홈페이지 입니다.</h1>
+<p>환영합니다.</p>
+
+</body>
+</html>
