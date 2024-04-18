1d0dbc1548bee40d77c4099f5c3c2254cdd14f9a
wild-mental <wild.mental@kidsnote.com>
2024-04-18 17:03:46 +0900

    DTO 적용 단계 1 : 적용 전 - User Entity 변경 시 영향 범위 확인 - Entity 와 DAO 관계

--- src/main/java/ac/su/springmvcinclass/domain/User.java
+++ src/main/java/ac/su/springmvcinclass/domain/User.java
@@ -4,9 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+@Entity  // Repository 에서 DB에 접근할 때 사용하는 객체 => DAO
 @Getter @Setter
-@Table(name = "users")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +17,13 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    // 요구사항에 따른 비즈니스 로직(BMI 계상) 을 위해서 데이터 읽어와야 함
+    // Repository 에서는 DAO 를 사용하지만,
+    // Service 에서 Controller 및 View 로 전달할 때는 DTO 를 써서 해결
+    @Column(nullable = true)
+    private double height;
+
+    @Column(nullable = true)
+    private double weight;
 }
--- src/main/java/ac/su/springmvcinclass/repository/UserRepository.java
+++ src/main/java/ac/su/springmvcinclass/repository/UserRepository.java
@@ -4,6 +4,6 @@ import ac.su.springmvcinclass.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository  // DAO (Data Access Object) 를 다루며 영속성을 관리 : DB 접근
 public interface UserRepository extends JpaRepository<User, Long> {
 }
