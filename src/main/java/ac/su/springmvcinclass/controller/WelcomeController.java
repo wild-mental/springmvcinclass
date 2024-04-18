bcbde78ef2ddc1bfc1ccacbefb420f18173efb70
wild-mental <wild.mental@kidsnote.com>
2024-04-18 10:57:38 +0900

    ModelAndView 방식과 Model to ViewName 방식 비교

--- src/main/java/ac/su/springmvcinclass/controller/WelcomeController.java
+++ src/main/java/ac/su/springmvcinclass/controller/WelcomeController.java
@@ -1,12 +1,32 @@
 package ac.su.springmvcinclass.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
     @GetMapping("/")
     public String welcome() {
         return "index";
+    }
+
+    // ModelAndView 방식
+    @GetMapping("/ex-return-mv")
+    public ModelAndView example() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("example-view");
+        modelAndView.addObject("name", "John");
+        modelAndView.addObject("age", 30);
+        return modelAndView;
+    }
+
+    // Model to ViewName 방식
+    @GetMapping("/ex-return-m")
+    public String example(Model model) {
+        model.addAttribute("name", "John");
+        model.addAttribute("age", 30);
+        return "example-view";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/example-view.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>내 이름은 ${name}이고 나이는 ${age}세입니다.</h1>
+<%-- 출력 : 내 이름은 John이고 나이는 30세입니다. --%>
+</body>
+</html>
